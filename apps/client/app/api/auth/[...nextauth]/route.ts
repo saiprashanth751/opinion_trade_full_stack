5ce8ffd1c8f6ba12ac9ead382c51bf1b6fba05a0
saiprashanth751 <saiprashanth751@gmail.com>
2025-07-07 12:12:12 +0530

    implemented-auth-provider

--- apps/client/app/api/auth/[...nextauth]/route.ts
+++ apps/client/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
 import NextAuth from "next-auth"
+import { authOptions } from "@/lib/auth"
 
+const handler = NextAuth(authOptions)
 
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-}
-
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST };

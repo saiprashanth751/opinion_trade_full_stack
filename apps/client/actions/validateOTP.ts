8b288defa1a14e08641c39b9d03bec96bad6b7b4
saiprashanth751 <saiprashanth751@gmail.com>
2025-07-12 22:43:39 +0530

    feat:setting-up-auth

---
+++ apps/client/actions/validateOTP.ts
@@ -0,0 +1,65 @@
+"use server"
+import prisma from "@repo/db/client"
+
+
+export const verifySMSOTPAction = async (phoneNumber: string, otp: string) => {
+    try {
+        const otpData = await prisma.OTP.findUnique({
+            where: {
+                otpID: phoneNumber,
+                otp
+            }
+        })
+
+        if (otpData?.isVerified) {
+            return {
+                verified: true,
+                message: "OTP already Verified",
+            }
+        }
+
+        if (!otpData) {
+            return {
+                verified: false,
+                message: "Phone Number is never verified"
+            }
+        }
+
+        if (otpData.expiresAt <= Date.now()) {
+            await prisma.OTP.delete({
+                where: {
+                    otpID: phoneNumber,
+                    otp
+                },
+                data: {
+                    otp
+                }
+            })
+            return {
+                verified: false,
+                message: "OTP expired"
+            }
+        }
+
+        await prisma.OTP.update({
+            where: {
+                otpID: phoneNumber,
+                otp
+            },
+            data: {
+                isVerified: true,
+                otp: ""
+            }
+        })
+        return {
+            verified: true,
+            message: "OTP Verified Successfully"
+        }
+    } catch (error) {
+        console.error("Error Verifying OTP: ", error);
+        return {
+            verified: false,
+            message: "An Error Occurred"
+        }
+    }
+}

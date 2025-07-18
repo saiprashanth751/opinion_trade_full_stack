e4d88cc1c4a4de94919675d6107e3323828a9f3b
saiprashanth751 <saiprashanth751@gmail.com>
2025-07-18 19:59:39 +0530

    implemented-server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,27 @@
 import express from "express"
+import { logger } from "@trade/logger"
+import orderRouter from "./router/orderRouter"
+import eventRouter from "./router/eventRouter"
+import morgan from "morgan"
 
 const app = express();
+const morganFormat = ":method :url :status :response-time ms";
+app.use(express.json());
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const [method, url, status, responseTime] = message.split(" ");
+                const formattedLog = `${method} - ${url} - ${status} - ${responseTime?.trim()} ms`;
+                logger.info(formattedLog)
+            },
+        },
+    })
+)
 
-app.listen(3000);
+app.use("/events", eventRouter);
+app.use("/order", orderRouter);
+
+app.listen(3001, () => {
+    logger.info("SERVER | Listening on port 3001")
+});
